6b6bff6b7c65970fee4c048d6317d6b71c1f97a8
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2022-11-17 10:38:25 +0100

    Update q1

--- src/com/tbz/q1/Q1.java
+++ src/com/tbz/q1/Q1.java
@@ -1,7 +1,24 @@
 package com.tbz.q1;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Q1 {
-    public void Start(){
+    private int random;
 
+    public void Start() {
+        Random random = new Random();
+        this.random = random.nextInt(100);
+        while (true) {
+            System.out.println("Type in a number");
+            int input = new Scanner(System.in).nextInt();
+            if (input == this.random) {
+                System.out.println("Congrats, you guessed the number!");
+                return;
+            } else if (input > this.random) {
+                System.out.println("Guess is too big");
+            } else {
+                System.out.println("Guess is too small");
+            }
+        }
     }
 }
