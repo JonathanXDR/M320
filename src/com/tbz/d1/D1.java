7e4e162286ae345f2850b67872f3e1383bd4d4be
Pascal Rieder <pascal.rieder@outlook.com>
2022-11-17 09:42:08 +0100

    update d1.java

--- src/com/tbz/d1/D1.java
+++ src/com/tbz/d1/D1.java
@@ -1,9 +1,23 @@
 package com.tbz.d1;
 
 public class D1 {
-    public void Start(){
+    public void Start() {
+        Konto konto1 = new Konto();
+        Konto konto2 = new Konto();
+        Konto konto3 = new Konto();
 
-        
-        //        System.out.println(student.getName());
+        konto1.ueberweisen(konto2, 30f);
+
+        konto2.einzahlen(200);
+        konto2.ueberweisen(konto3, 150f);
+
+        System.out.println("Konto 1");
+        System.out.println(konto1.getSaldo());
+
+        System.out.println("Konto 2");
+        System.out.println(konto2.getSaldo());
+
+        System.out.println("Konto 3");
+        System.out.println(konto3.getSaldo());
     }
 }
