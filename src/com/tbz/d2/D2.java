695d1517ff52cecaf4fd900a40b77fc2e6a332a9
Pascal Rieder <pascal.rieder@outlook.com>
2022-12-01 11:24:00 +0100

    update D2

--- src/com/tbz/d2/D2.java
+++ src/com/tbz/d2/D2.java
@@ -1,17 +1,19 @@
 package com.tbz.d2;
 
 public class D2 {
+    Flight flight = new Flight();
+
     public void Start() {
-        Flight flight = new Flight();
 
-        Passenger passenger1 = new Passenger();
-        passenger1.setName("John");
-        flight.addPassenger(passenger1);
+        createPassanger("Jane");
 
-        Passenger passenger2 = new Passenger();
-        passenger2.setName("Jane");
-        flight.addPassenger(passenger2);
 
         flight.displayPassengers();
+    }
+
+    private void createPassanger(String name) {
+        Passenger passenger2 = new Passenger();
+        passenger2.setName(name);
+        flight.addPassenger(passenger2);
     }
 }
