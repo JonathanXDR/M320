221e82a7cfe270c4345c76da4cb58bef5fc2c950
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2022-12-08 09:05:28 +0100

    Add Names to Team

--- src/com/tbz/v2/Team.java
+++ src/com/tbz/v2/Team.java
@@ -7,28 +7,28 @@ public class Team {
     private ArrayList<Player> team = new ArrayList<>();
 
     public Team(){
-        team.add(new Goalkeeper(""));
+        team.add(new Goalkeeper("Urs"));
 
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
-        team.add(new Striker(""));
+        team.add(new Striker("Aron"));
+        team.add(new Striker("Boris"));
+        team.add(new Striker("Cedric"));
+        team.add(new Striker("Dennis"));
+        team.add(new Striker("Elias"));
+        team.add(new Striker("Felix"));
+        team.add(new Striker("Gustav"));
+        team.add(new Striker("Hans"));
+        team.add(new Striker("Ivan"));
+        team.add(new Striker("Johannes"));
+        team.add(new Striker("Karl"));
+        team.add(new Striker("Lukas"));
+        team.add(new Striker("Max"));
+        team.add(new Striker("Niklas"));
+        team.add(new Striker("Oskar"));
+        team.add(new Striker("Paul"));
 
-        team.add(new Defender(""));
-        team.add(new Defender(""));
-        team.add(new Defender(""));
-        team.add(new Defender(""));
+        team.add(new Defender("Quentin"));
+        team.add(new Defender("Raphael"));
+        team.add(new Defender("Samuel"));
+        team.add(new Defender("Tobias"));
     }
 }
