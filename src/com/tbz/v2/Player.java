5e3a57a82e29a9aec52b9ff7a68f15ff388da49a
Pascal Rieder <pascal.rieder@outlook.com>
2022-12-08 08:35:43 +0100

    Add Player.java

---
+++ src/com/tbz/v2/Player.java
@@ -0,0 +1,13 @@
+package com.tbz.v2;
+
+public class Player {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
