1f64f14614794b9ed327cb2d00657832ef37f47a
Pascal Rieder <pascal.rieder@outlook.com>
2023-01-30 16:19:04 +0100

    Update OrderTerminal.java

--- src/com/tbz/project/OrderTerminal.java
+++ src/com/tbz/project/OrderTerminal.java
@@ -6,29 +6,22 @@ import java.util.Scanner;
 public class OrderTerminal {
 
     private ArrayList<Product> products = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
 
     public void Start() {
         System.out.println("Welcome to the Order Terminal");
-        System.out.println("On the menu today:");
-        System.out.println("------------------");
-        var todaysMenu = MenuCard.getInstance().getProducts();
-        for (var product : todaysMenu)
-            System.out.println(product.getName());
-        System.out.println("------------------");
-
-
-
-        Scanner scanner = new Scanner(System.in);
+        printMenue();
 
         while (true) {
             printOrder();
 
             System.out.println("Please select an option:");
             System.out.println("1. Add Item");
-            System.out.println("2. Go to Payment");
-            System.out.println("3. Abort Order");
+            System.out.println("2. Remove Item");
+            System.out.println("3. Go to Payment");
+            System.out.println("4. Abort Order");
 
-            int option = scanner.nextInt();
+            int option = readInt("", 1, 4);
 
             switch (option) {
                 case 1:
@@ -36,10 +29,11 @@ public class OrderTerminal {
                     var availableProducts = printAvailableProducts();
 
                     // Select product
-                    int input = scanner.nextInt();
+                    int input = readInt("", 1, availableProducts.size());
                     Product selectedProduct = availableProducts.get(input - 1);
 
                     if (selectedProduct.hasAvailableToppings()) {
+                        System.out.println("Toppings:");
                         var availableToppings = selectedProduct.getAvailableToppings();
                         while (true) {
                             for (int i = 0; i < availableToppings.size(); i++) {
@@ -48,7 +42,7 @@ public class OrderTerminal {
                             System.out.println((availableToppings.size() + 1) + ". No more toppings");
 
                             // Select toppings
-                            int toppingInput = scanner.nextInt();
+                            int toppingInput = readInt("Please select toppings", 1, availableToppings.size() + 1);
                             if (toppingInput == availableToppings.size() + 1) {
                                 break;
                             }
@@ -65,14 +59,32 @@ public class OrderTerminal {
 
                     System.out.println("Product added!");
                     break;
-                case 2:
-                    // Go to payment
-                    break;
+                    case 2:
+                        // Remove product from order
+                        // Print all products
+                        for(int i = 0; i < products.size(); i++) {
+                            System.out.println((i + 1) + ". " + products.get(i).getName());
+                        }
+                        // Select product
+                        int removeInput = readInt("Select Product that you want to remove", 1, products.size());
+                        products.remove(removeInput - 1);
+                        break;
                 case 3:
+                    // Go to payment
+                    var totalPrice = printOrder();
+                    if (totalPrice > 0) {
+                        System.out.println("Please pay " + totalPrice + "€");
+                    } else {
+                        System.out.println("You have nothing to pay!");
+                    }
+                    System.out.println("Thank you for your order!");
+                    return;
+                case 4:
                     // Exit
+                    System.out.println("Order aborted!");
                     return;
                 default:
-                    System.out.println("Invalid input");
+                    System.out.println("Invalid input; Try again!");
                     break;
             }
         }
@@ -80,20 +92,36 @@ public class OrderTerminal {
 
     }
 
-    private void printOrder() {
-        if (products.size() == 0) {
-            System.out.println("There are no products in the order");
-        } else {
-            System.out.println("Current Order:");
+
+    private void printMenue() {
+        System.out.println("------------------");
+        System.out.println("On the menu today:");
+        var todaysMenu = MenuCard.getInstance().getProducts();
+        for (var product : todaysMenu)
+            System.out.println(product.getName());
+        System.out.println("------------------");
+    }
+
+    private float printOrder() {
+        float totalPrice = 0;
+
+        if (products.size() != 0) {
             System.out.println("-----------------------");
+            System.out.println("Current Order:");
+
             for (Product product : products) {
-                System.out.println(product.getName());
+                System.out.println(product.getName() + " " + product.getPrice() + " CHF");
+                totalPrice += product.getPrice();
                 for (Topping topping : product.toppings) {
-                    System.out.println(" - " + topping.getName());
+                    System.out.println(" - " + topping.getName() + " " + topping.getPrice() + " CHF");
+                    totalPrice += topping.getPrice();
                 }
             }
+            System.out.println("Total: " + totalPrice + " CHF");
             System.out.println("-----------------------");
+            return totalPrice;
         }
+        return 0.0f;
     }
 
     private ArrayList<Product> printAvailableProducts() {
@@ -103,6 +131,24 @@ public class OrderTerminal {
             System.out.println((i + 1) + ". " + availableProducts.get(i).getName());
         }
         return availableProducts;
+    }
+
+    private int readInt(String text, int min, int max) {
+        System.out.println(text);
+        while (true) {
+
+            if (scanner.hasNextInt()) {
+                int input = scanner.nextInt();
+                if (input >= min && input <= max) {
+                    return input;
+                } else {
+                    System.out.println("Invalid input; Try again!");
+                }
+            } else {
+                scanner.next();
+                System.out.println("Invalid input; Try again!");
+            }
+        }
     }
 }
 
